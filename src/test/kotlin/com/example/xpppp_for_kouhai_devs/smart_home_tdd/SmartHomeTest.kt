0986f12f7dfd582640ea92fc3fcd51e24107e340
Atsushi Suganami <atsushi_suganami@mail.toyota.co.jp>
2024-12-12 21:09:55 +0900

    SmartHomeTestにテストタイトルのみ追加
    Co-authored-by: Ryozo Ihara <ryozo_ihara@mail.toyota.co.jp>

--- src/test/kotlin/com/example/xpppp_for_kouhai_devs/smart_home_tdd/SmartHomeTest.kt
+++ src/test/kotlin/com/example/xpppp_for_kouhai_devs/smart_home_tdd/SmartHomeTest.kt
@@ -1,7 +1,38 @@
-package com.example.xpppp_for_kouhai_devs.smart_home_tdd
+package com.example.xpppp_for_senpai_devs.smart_home_tdd
+
+import org.junit.jupiter.api.Test
 
 class SmartHomeTest {
-    //ここにテストを記述してください。
-    //必要なTestDoubleは自分たちで設定しましょう。
-    //TestDoubleを作るのにmockkなどの便利なライブラリがありますが今回は使用しないでください。
+    @Test
+    fun `システムを起動したら、「照明を点灯しました」を表示する`() {
+
+    }
+
+    @Test
+    fun `isOnCounterが５になったら、bulbWarningをtrueにする`() {
+
+    }
+
+    @Test
+    fun `isOnCounterが0の時、システムを起動したら、bulbWarningをfalseにする`() {
+
+    }
+
+    @Test
+    fun `bulbWarningがTrueの時、システムを起動すると「電球を交換してください。」を表示する`() {
+
+    }
+
+    @Test
+    fun `電球交換したら、isOnCounterResetをよぶ`() {
+
+    }
+
+    @Test
+    fun `システムを起動したら、10秒後に「照明を消灯しました」を表示する`() {
+
+    }
+    //参考リンク: https://www.baeldung.com/kotlin/wait-sleep-delay-difference
 }
+
+
